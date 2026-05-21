ac4e658afeee234c7e224a047e5d4fced062d6b3
SkrimanS <36265694+SkrimanS@users.noreply.github.com>
2026-05-21 13:29:19 +0300

    Bump core version to 0.9.7

--- include/clc/core/Version.hpp
+++ include/clc/core/Version.hpp
@@ -11,11 +11,11 @@ struct Version final {
 };
 
 [[nodiscard]] constexpr Version core_version() noexcept {
-    return Version{0, 9, 6};
+    return Version{0, 9, 7};
 }
 
 [[nodiscard]] constexpr std::string_view core_version_string() noexcept {
-    return "0.9.6";
+    return "0.9.7";
 }
 
 } // namespace clc
