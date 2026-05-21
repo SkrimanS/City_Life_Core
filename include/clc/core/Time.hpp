5f4e9df0beb756a8d729933e7cb875f0ab173ad9
SkrimanS <36265694+SkrimanS@users.noreply.github.com>
2026-05-21 16:35:53 +0300

    Add time unit tick helpers

--- include/clc/core/Time.hpp
+++ include/clc/core/Time.hpp
@@ -15,4 +15,36 @@ private:
     Tick tick_{};
 };
 
+[[nodiscard]] constexpr GameTime::Tick ticks_per_second() noexcept {
+    return 1;
+}
+
+[[nodiscard]] constexpr GameTime::Tick ticks_per_minute() noexcept {
+    return 60 * ticks_per_second();
+}
+
+[[nodiscard]] constexpr GameTime::Tick ticks_per_hour() noexcept {
+    return 60 * ticks_per_minute();
+}
+
+[[nodiscard]] constexpr GameTime::Tick ticks_per_day() noexcept {
+    return 24 * ticks_per_hour();
+}
+
+[[nodiscard]] constexpr GameTime::Tick seconds_to_ticks(std::uint64_t seconds) noexcept {
+    return seconds * ticks_per_second();
+}
+
+[[nodiscard]] constexpr GameTime::Tick minutes_to_ticks(std::uint64_t minutes) noexcept {
+    return minutes * ticks_per_minute();
+}
+
+[[nodiscard]] constexpr GameTime::Tick hours_to_ticks(std::uint64_t hours) noexcept {
+    return hours * ticks_per_hour();
+}
+
+[[nodiscard]] constexpr GameTime::Tick days_to_ticks(std::uint64_t days) noexcept {
+    return days * ticks_per_day();
+}
+
 } // namespace clc
