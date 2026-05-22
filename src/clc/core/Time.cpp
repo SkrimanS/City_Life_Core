365822e9e0cb86a0f068ac3b06e5ab2c094da19d
SkrimanS <36265694+SkrimanS@users.noreply.github.com>
2026-05-22 10:51:09 +0300

    Saturate GameTime advance on overflow

--- src/clc/core/Time.cpp
+++ src/clc/core/Time.cpp
@@ -1,12 +1,23 @@
 #include "clc/core/Time.hpp"
 
+#include <limits>
+
 namespace clc {
 
 GameTime::Tick GameTime::current_tick() const noexcept {
     return tick_;
 }
 
+bool GameTime::can_advance(Tick ticks) const noexcept {
+    return ticks <= std::numeric_limits<Tick>::max() - tick_;
+}
+
 void GameTime::advance(Tick ticks) noexcept {
+    if (!can_advance(ticks)) {
+        tick_ = std::numeric_limits<Tick>::max();
+        return;
+    }
+
     tick_ += ticks;
 }
 
