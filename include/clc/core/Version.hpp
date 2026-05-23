ca7edc4886a219209e2f8cf5fdb05e81191344c8
SkrimanS <36265694+SkrimanS@users.noreply.github.com>
2026-05-23 14:46:06 +0300

    chore: update core version API to 1.0.0

--- include/clc/core/Version.hpp
+++ include/clc/core/Version.hpp
@@ -11,11 +11,11 @@ struct Version final {
 };
 
 [[nodiscard]] constexpr Version core_version() noexcept {
-    return Version{0, 9, 9};
+    return Version{1, 0, 0};
 }
 
 [[nodiscard]] constexpr std::string_view core_version_string() noexcept {
-    return "0.9.9";
+    return "1.0.0";
 }
 
 } // namespace clc
